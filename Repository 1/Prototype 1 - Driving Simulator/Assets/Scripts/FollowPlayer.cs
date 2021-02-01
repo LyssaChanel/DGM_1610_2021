c576ceb5ec7fef81ffa5c60d7ccb033b888b874d
Alyssa Simmonds <108785952MY.UVU.EDU>
2021-02-01 15:27:01 -0700

    Fixed Camera and Speed

--- Repository 1/Prototype 1 - Driving Simulator/Assets/Scripts/FollowPlayer.cs
+++ Repository 1/Prototype 1 - Driving Simulator/Assets/Scripts/FollowPlayer.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Move : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
 {
+    public GameObject player;
+    private Vector3 offset = new Vector3(0,7,-11);
+
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,6 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       transform.position = player.transform.position + offset; 
     }
 }
