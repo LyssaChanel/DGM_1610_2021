8e72762179ab72d228988147fabe1f59b2f8360f
Alyssa Simmonds <108785952MY.UVU.EDU>
2021-02-11 18:45:56 -0700

    Rotation

--- Repository 1/Challenge 1 - Flying a Plane/Challenge 1/Assets/Challenge 1/Scripts/PropellerGoMove.cs
+++ Repository 1/Challenge 1 - Flying a Plane/Challenge 1/Assets/Challenge 1/Scripts/PropellerGoMove.cs
@@ -13,6 +13,6 @@ public class PropellerGoMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       transform.Rotate (new Vector3 (0,0,40 *Time.deltaTime));
+       transform.Rotate (new Vector3 (0,0,700 *Time.deltaTime));
     }
 }
