c1a5331a2518fd8ff54b7d974f7eaf675e35cb40
Alyssa Simmonds <108785952MY.UVU.EDU>
2021-03-01 11:43:47 -0700

    Random Spawns and Destroy Range

---
+++ Repository 1/Prtotype 2 - Stampeed/Prototype 2 - Stampeed/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    private float spawnRangeX = 20;
+    private float spawnRangeZ = 20;
+    public GameObject[] animalPrefabs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //randomly generate animals on x axis
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),0, spawnRangeZ);
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        if(Input.GetKeyDown(KeyCode.S)){
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+        }
+    }
+}
